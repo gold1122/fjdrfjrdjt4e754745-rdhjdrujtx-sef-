7b4a397ebf5c9cd2eb484c61572e15614afd0e24
gold1122 <91434901+gold1122@users.noreply.github.com>
2022-05-26 14:22:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,98 +1,133 @@
-const express = require('express');
-const app = express();
-app.listen(() => console.log('Made by Megz'));
-app.use('/ping', (req, res) => {
-    res.send(new Date());
+const { Client, Intents } = require("discord.js");
+const client = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MEMBERS,
+        Intents.FLAGS.GUILD_BANS,
+        Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+        Intents.FLAGS.GUILD_INTEGRATIONS,
+        Intents.FLAGS.GUILD_WEBHOOKS,
+        Intents.FLAGS.GUILD_INVITES,
+        Intents.FLAGS.GUILD_VOICE_STATES,
+        Intents.FLAGS.GUILD_PRESENCES,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+        Intents.FLAGS.GUILD_MESSAGE_TYPING,
+        Intents.FLAGS.DIRECT_MESSAGES,
+        Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+        Intents.FLAGS.DIRECT_MESSAGE_TYPING
+    ]
 });
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const cmd = require('node-cmd');
-const ms = require('ms');
-const fs = require('fs');
-const ytdl = require('ytdl-core');
-const canvas = require('canvas');
-const convert = require('hh-mm-ss');
-const fetchVideoInfo = require('youtube-info');
-const simpleytapi = require('simple-youtube-api');
-const util = require('util');1
-const gif = require('gif-search');
-const jimp = require('jimp');
-const guild = require('guild');
-const hastebins = require('hastebin-gen');
-const getYoutubeID = require('get-youtube-id');
-const pretty = require('pretty-ms');
-const moment = require('moment');
-const request = require('request');
-const dateFormat = require('dateformat');
-const fetch = require('node-fetch');
-client.on('ready', async () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-    client.user.setStatus('idle');
-    client.user.setActivity(''); 
+const { joinVoiceChannel } = require('@discordjs/voice');
+
+const { Client: music, Queue } = require("./src/index");
+const player = new music({ client, lang: "ar-EG" });
+let token = Token
+let channel = '910946852269088812'
+client.on("ready", async () => {
+    let voiceChannel = client.channels.cache.get(channel);
+    if (voiceChannel) {
+      const connection = await joinVoiceChannel({
+        channelId: voiceChannel.id,
+        guildId: voiceChannel.guild.id,
+        adapterCreator: voiceChannel.guild.voiceAdapterCreator,
+        selfDeaf: false,
+      });
+      connection;
+    }
+})
+client.on("messageCreate", async(msg) => {
+    console.log(Queue.get(msg.guildId) ? Queue.get(msg.guildId).loop : false)
+    if ("Krypton000" == "cyber") console.log('whoot???');
+    else if (msg.content.startsWith("!play")) player.play(msg, msg.content.split(' ').slice(1).join(' '));
+    else if (msg.content.startsWith("!stop")) player.stop(msg);
+    // else if (msg.content.startsWith("!pause")) player.pause(msg);
+    else if (msg.content.startsWith("!resume")) player.resume(msg);
+    else if (msg.content.startsWith("!skip")) player.skip(msg);
+    else if (msg.content.startsWith("!volume")) player.volume(msg, msg.content.split(' ')[1]);
+    else if (msg.content.startsWith("!search")) player.search(msg, msg.content.split(' ').slice(1).join(' '))
+    else if (msg.content.startsWith("!loop")) player.loop(msg, msg.content.split(' ')[1]);
+    else if (msg.content.startsWith("!connect")) player.connect(msg);
+    else if (msg.content.startsWith("!disconnect")) player.disconnect(msg);
 });
-client.login(process.env.BOT_TOKEN);
-
- ///////////////////////////////////////////////////////////////////////
- client.on('message', msg=>{
-    let SS = msg.content.split(" ").slice('').join(" ")
-
-  var channel =  msg.channel.id === '925435160403537970'
-  if (!channel) return false;
-  if(msg.author.bot)return;
-  if(msg.content.startsWith(''))
- {
-        msg.delete()
-
-  var embed = new Discord.MessageEmbed()
-  .setAuthor(msg.author.username,msg.author.avatarURL({dynamic: true }))
-  .setThumbnail(msg.author.avatarURL())
-  .setColor('#000')
-  .setDescription(`\n
-  **${SS}**`)
-   .setFooter(msg.guild.name,msg.guild.iconURL({dynamic: true }))
-    .setTimestamp(msg.numberDate);
-
-  client.channels.cache.get("925435160403537970").send(embed).then(function (msg) {
-
-              msg.react("941428253929902081")
-              msg.react("941428252097015818")
-            }).catch(function() {
-
-             });
-}
-
-
+client.on("speech", (msg) => {
+    if (msg.content) {
+        msg.author.send(msg.content)
+        // console.log(msg.content)
+        if ("Krypton000" == "cyber") console.log('whoot???');
+        else if (msg.content.includes("play") || msg.content.includes("شغل")) player.play(msg, msg.content.split(' ').slice(' ').join(' '));
+        else if (msg.content.includes("stop") || msg.content.includes("وقف")) player.stop(msg);
+        // else if (msg.content.includes("pause") || msg.content.includes("اسكت")) player.pause(msg);
+        else if (msg.content.includes("resume") || msg.content.includes("كمل")) player.resume(msg);
+        else if (msg.content.includes("skip") || msg.content.includes("الي بعده")) player.skip(msg);
+    }
 });
-/*
-client.on('message', msg=>{
-    let SS = msg.content.split(" ").slice('').join(" ")
+player.events
+    .on("disconnected", (msg, connection, voiceChannel) => {
+        let channel = msg.channel;
+        if (channel.type == "dm") return;
+        channel.send ? channel.send({ content: `bot disconnected to <#${voiceChannel.id}>` }) : "bad reading";
+    })
+    .on("connected", (msg, connection, voiceChannel) => {
+        let channel = msg.channel;
+        if (channel.type == "dm") return;
+        channel.send ? channel.send({ content: `bot connected to <#${voiceChannel.id}>` }) : "bad reading";
+    })
+    .on("playSong", (msg, song) => {
+        console.log(song)
+        let channel = msg.channel;
+        if (channel.type == "dm") return;
+        channel.send ? channel.send({ content: `**${song.title}** is playing!` }) : "bad reading";
+    })
+    .on("addSong", (msg, song) => {
+        let channel = msg.channel;
+        if (channel.type == "dm") return;
+        channel.send ? channel.send({ content: `**${song.title}** got added!` }) : "bad reading";
+    })
+    .on("stopSong", async (msg) => {
+        let channel = msg.channel;
+        if (channel.type == "dm") return;
+        channel.send ? channel.send({ content: `Music got stoped!.` }) : "bad reading";
+        let voiceChannel = client.channels.cache.get(channel);
+        if (voiceChannel) {
+          const connection = await joinVoiceChannel({
+            channelId: voiceChannel.id,
+            guildId: voiceChannel.guild.id,
+            adapterCreator: voiceChannel.guild.voiceAdapterCreator,
+            selfDeaf: false,
+          });
+          connection;
+        }
+    })
+    .on("resumeSong", (msg) => {
+        let channel = msg.channel;
+        if (channel.type == "dm") return;
+        channel.send ? channel.send({ content: `Music got resumed!.` }) : "bad reading";
+    })
+    // .on("pauseSong", (msg) => {
+    //     let channel = msg.channel;
+    //     if (channel.type == "dm") return;
+    //     channel.send ? channel.send({ content: `Music got paused!.` }) : "bad reading";
+    // })
+    .on("volumeSong", (msg, percentage) => {
+        let channel = msg.channel;
+        if (channel.type == "dm") return;
+        channel.send ? channel.send({ content: `Music volume has changed to: ${percentage}%` }) : "bad reading";
+    })
+    .on("skipSong", (msg) => {
+        let channel = msg.channel;
+        if (channel.type == "dm") return;
+        channel.send ? channel.send({ content: `Music got skiped!.` }) : "bad reading";
+    })
+    .on("loopSong", (msg, arg) => {
+        let channel = msg.channel.type;
+        if (channel.type == "dm") return;
+        channel.send ? channel.send({ content: `Music got looped ${arg}!.` }) : "bad reading";
+    })
+    .on("search", (msg, arg, result) => {
+        let channel = msg.channel;
+        if (channel.type == "dm") return;
+        channel.send(result.map((video, index) => `#${index} - ${video.url}`).join("\n"))
+    });
 
-  var channel =  msg.channel.id === '922128101364858910'
-  if (!channel) return false;
-  if(msg.author.bot)return;
-  if(msg.content.startsWith(''))
- {
-        msg.delete()
-
-  var embed = new Discord.MessageEmbed()
-  .setAuthor(msg.author.tag)
-  .setThumbnail(msg.author.avatarURL())
-  .setColor('Random')
-  .setDescription(`**suggestion**  :\n
-  ${SS}`);
-
- 
-
-  client.channels.cache.get("922128101364858910").send(embed).then(function (msg) {
-
-              msg.react("✅")
-              msg.react("❎")
-            }).catch(function() {
-
-             });
-}
-
-
-});
-*/
- ///////////////////////////////////////////////////////////////////////
+client.login(token);
